6f87894cdd26b51c486f334fa8371463bc8ada0a
marco <ttap5090@gmail.com>
2022-05-14 16:02:41 -0300

    Upando atividade

---
+++ TesteCurso.java
@@ -0,0 +1,33 @@
+package senaicursos;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TesteCurso {
+	private WebDriver driver;
+	@Before
+	public void abrirNavegador() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files (x86)\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+@Test
+	public void testeNavegador() {
+		driver.get("https://sp.senai.br/");
+		driver. findElement(By.id("s1")).sendKeys("gestão");
+		driver. findElement(By.id("search")).click();
+	}
+	
+/*@Test
+	public void testeNavegador() {
+		driver.get("https://informatica.sp.senai.br/");
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.findElement(By.id("Busca1_txtFiltro")).sendKeys("gestão");
+		driver. findElement(By.id("Busca1_btnBusca")).click();
+	}*/
+}
